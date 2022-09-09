efad515901ec255413260256701fbd0b2f4e7556
NguyenTrungDoan <doankhtn20@gmail.com>
2022-09-09 18:27:34 +0700

    bc

--- axios-qlps/app/controller/qlsp.js
+++ axios-qlps/app/controller/qlsp.js
@@ -86,3 +86,14 @@ function addProduct() {
       console.log(error);
     });
 }
+
+function deleteProduct(id) {
+  service.deleteProductApi(id)
+  .then(function () {
+    //render list data
+    fetchData();
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
